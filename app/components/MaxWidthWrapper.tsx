6da3a228811e09c92c2c2fa62888456fc21a7df1
Souraevshing <isauravshing@gmail.com>
2025-05-04 11:27:06 +0530

    🎉 (initial code): init
    feat: add initial code

---
+++ app/components/MaxWidthWrapper.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return (
+    <div
+      className={` mx-auto w-full max-w-screen px-2.5 md:px-20 ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
