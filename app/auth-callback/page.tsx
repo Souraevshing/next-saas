e8760447aa6a23afc70a80acd7d065461ece7e37
Souraevshing <isauravshing@gmail.com>
2025-05-05 23:56:53 +0530

    👽️ (api): auth
    feat: add auth api completed

---
+++ app/auth-callback/page.tsx
@@ -0,0 +1,10 @@
+import { useRouter, useSearchParams } from "next/navigation";
+
+const Auth = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  return <div>page</div>;
+};
+
+export default Auth;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,20 @@
-const page = () => {
-  return <div>page</div>;
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
+
+const Dashboard = async () => {
+  const { getUser } = getKindeServerSession();
+
+  const user = await getUser();
+
+  if (!user) {
+    redirect("/auth-callback?origin=dashboard");
+  }
+
+  return (
+    <>
+      <div>{user.email}</div>
+    </>
+  );
 };
 
-export default page;
+export default Dashboard;
